783ae15ea039873f8480f8b9e1b5a7d0ca145662
Clem-Fern <clemfern456@gmail.com>
2020-11-29 11:58:58 +0100

    Remove vip field & admin role in config

--- lua/guthwhitelistsystem/sh_config.lua
+++ lua/guthwhitelistsystem/sh_config.lua
@@ -10,20 +10,12 @@ guthwhitelistsystem.TimerSaveTime   =   120
 --  < In bits, (network optimisation), how much job ID is maximum in bits (don't touch it if
 --  < you don't need/know). For example, 7 bits accept 127 in maximum and 0 in minimum. If you
 --  < have more than 127 jobs, up this number.
-guthwhitelistsystem.JobIDBit       =   7
-
---  < All the VIP ranks, they have access to the VIP whitelist
-guthwhitelistsystem.VIPRanks        =
-                    {
-                        ["superadmin"] = true,
-                        ["vip"] = true,
-                    }
+guthwhitelistsystem.JobIDBit       =   8
 
 --  < All the Admin ranks, they have access to whitelist panel (they can whitelist peoples and jobs)
 guthwhitelistsystem.AdminRanks        =
                     {
                         ["superadmin"] = true,
-                        ["admin"] = true,
                     }
 
 --  < Language: choice between 'en' and 'fr'
