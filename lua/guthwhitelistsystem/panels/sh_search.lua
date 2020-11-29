bb940749301b3d6dd8aaf34c85e8cb621705f0e9
Clem-Fern <clemfern456@gmail.com>
2020-11-29 11:59:35 +0100

    Remove empty search panel

--- lua/guthwhitelistsystem/panels/sh_search.lua
+++
@@ -1,9 +0,0 @@
-if not CLIENT then return end
-
-guthwhitelistsystem.setPanel( "Search", "icon16/report_magnify.png", math.huge, function()
-
-    local pnlS = vgui.Create( "DPanel" ) -- panel search
-
-    return pnlS
-
-end )
